3517f518b590cb71e791c911410493c53cbe5d83
Harena-Git <faniriantsoaharena@gmail.com>
2025-10-01 12:16:30 +0100

    Reformation Central

--- DepositAccount/obj/Debug/net8.0/DepositAccount.AssemblyInfo.cs
+++ DepositAccount/obj/Debug/net8.0/DepositAccount.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DepositAccount")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d86f06add76b093985cb289753262bb7b055762")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+618782d69ec378ed17fb91d1723c7590150b8de5")]
 [assembly: System.Reflection.AssemblyProductAttribute("DepositAccount")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DepositAccount")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
